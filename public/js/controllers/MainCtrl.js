c15636891ca0f11d83ba739ae237af14e09cc131
paulngouchet <ngouchet@bu.edu>
2020-08-02 21:56:31 -0400

    Clean Code

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,6 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
-
 	$scope.tagline = 'To the moon and back!';
-
+	
 	$scope.search = function() {
 		$scope.tagline = 'Fake Duck Duck Go';
 		console.log("clicked")
