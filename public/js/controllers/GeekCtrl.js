fcaf1a9f14e05dbfe53221697e4c782515b0b767
paulngouchet <ngouchet@bu.edu>
2020-08-02 21:54:18 -0400

    Clean Code

--- public/js/controllers/GeekCtrl.js
+++ public/js/controllers/GeekCtrl.js
@@ -1,23 +1,14 @@
 angular.module('GeekCtrl', []).controller('GeekController', function($scope,  $sce) {
-
 	$scope.tagline = 'The square root of life is pi!';
-
 	$scope.to_search = "Facebook"
-
 	$scope.name = ""
 	$scope.prediction = ""
-
 	$scope.url = $sce.trustAsResourceUrl("https://techcrunch.com/")
-
+	
 	$scope.search = function() {
-
 		$scope.name = $scope.to_search
 		$scope.prediction = "IPO"
-
 		var display = "https://techcrunch.com/tag/" + $scope.to_search + "/"
-
 		$scope.url = $sce.trustAsResourceUrl(display)
-
 	};
-
 });
